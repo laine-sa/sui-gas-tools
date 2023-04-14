acd486e2bc314f703a39a2adb8fd288d993e11e8
Knox Hutchinson <knox@knoxsdata.com>
2023-04-14 10:06:17 -0500

    ordering logic

--- runner_testnet.ts
+++ runner_testnet.ts
@@ -3,34 +3,44 @@ const validators: any = require('./validators.json').result.activeValidators
 const { spawn, exec } = require('node:child_process')
 const env: any = require('./env.json')
 
-const scriptPrep: any = spawn('sh', ['./update_validators.sh'])
-scriptPrep.stderr.on('data', (data: string) => {
-    console.error(`child stderr:\n${data}`);
-})
-const validator = validators.find((v: any) => v.suiAddress === env.ACTIVE_ADD_TESTNET)
-
-let nrgp = nextReferenceGasPrice(validators)
 
 
 
-if (validator.gasPrice == nrgp * 0.98) {
-    console.log('Reference Gas Price is correct')
-}
-else {
-    console.log('Reference Gas Price is incorrect')
-    console.log(nrgp)
-    nrgp = nrgp * 0.98
-    console.log(`setting reference gas price to: ${nrgp}`)
-    const command: string = `/home/sui/sui/target/debug/sui validator update-gas-price ${nrgp}`
-    // loop over each word in command and place each word in an array
-    const args: string[] = command.split(' ')
-    const options: any = args.slice(1)
-    const child: any = spawn(args[0], options)
-    child.stdout.on('data', (data: string) => {
-        console.log(`child stdout:\n${data}`);
-    });
 
-    child.stderr.on('data', (data: string) => {
+export const starter = () => {
+    const scriptPrep: any = spawn('sh', ['./update_validators.sh'])
+    scriptPrep.stderr.on('data', (data: string) => {
         console.error(`child stderr:\n${data}`);
     })
+
 }
+
+export const ender = async () => {
+    await starter()
+    const validator = validators.find((v: any) => v.suiAddress === env.ACTIVE_ADD_TESTNET)
+
+    let nrgp = nextReferenceGasPrice(validators)
+    if (validator.gasPrice == nrgp * 0.98) {
+        console.log('Reference Gas Price is correct')
+    }
+    else {
+        console.log('Reference Gas Price is incorrect')
+        console.log(nrgp)
+        nrgp = nrgp * 0.98
+        console.log(`setting reference gas price to: ${nrgp}`)
+        const command: string = `/home/sui/sui/target/debug/sui validator update-gas-price ${nrgp}`
+        // loop over each word in command and place each word in an array
+        const args: string[] = command.split(' ')
+        const options: any = args.slice(1)
+        const child: any = spawn(args[0], options)
+        child.stdout.on('data', (data: string) => {
+            console.log(`child stdout:\n${data}`);
+        });
+
+        child.stderr.on('data', (data: string) => {
+            console.error(`child stderr:\n${data}`);
+        })
+    }
+}
+
+ender()
