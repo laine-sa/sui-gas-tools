76addbb54d285ef38325ab8bfc41f3d8a48f78bd
Michael Hubbard <michaelh@laine.co.za>
2023-05-29 11:05:20 +0200

    Add run and env sample

---
+++ run.ts
@@ -0,0 +1,27 @@
+import { Validator } from './types'
+import { calcMax, calcMin, calcMean, calcMedian, calcWeightedMean, nextReferenceGasPrice } from './stats_helpers'
+const { exec } = require('node:child_process')
+const axios = require ('axios')
+require('dotenv').config()
+
+axios.post(process.env.RPC_URL,{jsonrpc: "2.0",id: 1,method: "suix_getLatestSuiSystemState" }, {
+    headers: {'Content-Type':'application/json'}
+})
+    .then((response: any) => {
+
+        const validators: [Validator] = response.data.result.activeValidators
+
+        console.log("==== Statistics on current validator set ====")
+        console.log("Total validators: "+validators.length)
+        console.log("Min Reference Gas Price: "+calcMin(validators))
+        console.log("Max Reference Gas Price: "+calcMax(validators))
+        console.log("Mean Reference Gas Price: "+calcMean(validators))
+        console.log("Stake Weighted Mean Reference Gas Price: "+calcWeightedMean(validators))
+        console.log("Median Reference Gas Price: "+calcMedian(validators))
+        console.log("Estimated Next Reference Gas Price: "+nextReferenceGasPrice(validators))
+    })
+    .catch((err: any) => {
+        console.error('Error processing: ', err)
+    })
+
+    
