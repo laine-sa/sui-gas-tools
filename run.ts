a626f39c215e11cad70db49856892ed823be9f51
Michael Hubbard <michaelh@laine.co.za>
2023-07-11 10:59:43 +0200

    Fix dependencies

--- run.ts
+++ run.ts
@@ -1,26 +1,31 @@
 import { Validator } from './types'
 import { calcMax, calcMin, calcMean, calcMedian, calcWeightedMean, nextReferenceGasPrice } from './stats_helpers'
-const axios = require ('axios')
+import axios from 'axios'
 require('dotenv').config()
 
-axios.post(process.env.RPC_URL,{jsonrpc: "2.0",id: 1,method: "suix_getLatestSuiSystemState" }, {
-    headers: {'Content-Type':'application/json'}
-})
-    .then((response: any) => {
+if(typeof(process.env.RPC_URL) == 'string') {
 
-        const validators: [Validator] = response.data.result.activeValidators
-
-        console.log("==== Statistics on current validator set ====")
-        console.log("Total validators: "+validators.length)
-        console.log("Min Reference Gas Price: "+calcMin(validators))
-        console.log("Max Reference Gas Price: "+calcMax(validators))
-        console.log("Mean Reference Gas Price: "+calcMean(validators))
-        console.log("Stake Weighted Mean Reference Gas Price: "+calcWeightedMean(validators))
-        console.log("Median Reference Gas Price: "+calcMedian(validators))
-        console.log("Estimated Next Reference Gas Price: "+nextReferenceGasPrice(validators))
+    axios.post(process.env.RPC_URL,{jsonrpc: "2.0",id: 1,method: "suix_getLatestSuiSystemState" }, {
+        headers: {'Content-Type':'application/json'}
     })
-    .catch((err: any) => {
-        console.error('Error processing: ', err)
-    })
-
+        .then((response: any) => {
     
+            const validators: [Validator] = response.data.result.activeValidators
+    
+            console.log("==== Statistics on current validator set ====")
+            console.log("Total validators: "+validators.length)
+            console.log("Min Reference Gas Price: "+calcMin(validators))
+            console.log("Max Reference Gas Price: "+calcMax(validators))
+            console.log("Mean Reference Gas Price: "+calcMean(validators))
+            console.log("Stake Weighted Mean Reference Gas Price: "+calcWeightedMean(validators))
+            console.log("Median Reference Gas Price: "+calcMedian(validators))
+            console.log("Estimated Next Reference Gas Price: "+nextReferenceGasPrice(validators))
+        })
+        .catch((err: any) => {
+            console.error('Error processing: ', err)
+        })
+    
+}
+else {
+    console.log("Please create a .env file with an RPC_URL value.")
+}
